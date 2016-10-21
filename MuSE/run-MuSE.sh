d7c6f04b08baff30a635525c15916943cdab5122
James Hawley <jamesrichardhawley@gmail.com>
2016-10-21 18:21:32 -0400

    Changing comments in MuSE wrapper

--- MuSE/run-MuSE.sh
+++ MuSE/run-MuSE.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#$ -S /bin/bash
 #$ -N MuSE
-#$ -j y
 #$ -cwd 
 #$ -q light.q 
 module load perl/5.18.1  
 module load R
 
-#mutations.sorted.bed IFC.output DHS.bed window cor
+#mutations.sorted.bed C3D.output DHS_reference.bed window cor
 
 perl MUSE-BETA-1.5-1-mutOnly.pl $1 $2 $3 $4 $5 
