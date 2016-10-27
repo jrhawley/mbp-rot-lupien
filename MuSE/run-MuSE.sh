15d86ae8cf19f37e1dec7a07974c3f5956cb103b
James Hawley <jamesrichardhawley@gmail.com>
2016-10-27 18:28:33 -0400

    Updating run-MuSE to have right perl script name

--- MuSE/run-MuSE.sh
+++ MuSE/run-MuSE.sh
@@ -2,10 +2,9 @@
 
 #$ -N MuSE
 #$ -cwd 
-#$ -q light.q 
+#$ -q lupiengroup
 module load perl/5.18.1  
 module load R
 
 #mutations.sorted.bed C3D.output DHS_reference.bed window cor
-
-perl MUSE-BETA-1.5-1-mutOnly.pl $1 $2 $3 $4 $5 
+perl MUSE-BETA-1.5-1.Share.pl $1 $2 $3 $4 $5 
